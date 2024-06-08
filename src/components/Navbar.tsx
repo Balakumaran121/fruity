dcf895b3124cc22372b24eb6f8dd0fae8eaeab46
Balakumaran <bkumaran121@gmail.com>
2024-06-08 07:37:46 +0530

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
           <input
             className="py-2 px-4 outline-none border border-gray-300 rounded-md w-full"
             type="text"
-            placeholder="I'm shoppinf for..."
+            placeholder="I'm shoppinf form..."
           />
           <button className="absolute uppercase top-0 right-0 bg-accent hover:bg-accentDark text-white text-md flex items-center gap-2 px-4 h-full rounded-r-md">
             Search
